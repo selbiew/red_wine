fb951af4408a778736793cecc2f5c487e2ee6915
William Selbie <wselbie6@gmail.com>
2018-12-28 21:11:04 -0800

    Initial commit.

---
+++ linear_regression.py
@@ -0,0 +1,11 @@
+import math
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sb
+
+def main():
+    print("Hello World!")
+
+if __name__ == "__main__":
+    main()
